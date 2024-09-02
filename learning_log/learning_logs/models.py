cb53a26876636b5d7f08831c4c038073c02ea760
ola pacia <olusiaop05@gmail.com>
2024-09-02 10:25:07 +0200

    define a model for topics

--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+# it shows the way the data will be trated
+
+# MODEL TO HANDLE THE TOPICS
+
+class Topic(models.Model):
+    """Topic that user gets to know"""
+    text = models.CharField(max_lenght=200)
+    data_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Returns representation of the model as a string"""
+        return self.text
