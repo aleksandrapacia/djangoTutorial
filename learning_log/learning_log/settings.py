d4cc58b955943ae9b7485c80a77a4aac8130ec0f
ola pacia <olusiaop05@gmail.com>
2024-09-02 11:41:23 +0200

    admin site, entries and topics, if topic's entry is shorter that 50 then do not display

--- learning_log/learning_log/settings.py
+++ learning_log/learning_log/settings.py
@@ -31,6 +31,11 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+
+    #My apps
+    'learning_logs',
+
+    # Default apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
